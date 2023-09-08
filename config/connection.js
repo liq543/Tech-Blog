dc8de8ec24ceb4425eb179d5b8448d8e4c851f54
unknown <liq543@gmail.com>
2023-09-08 15:18:46 -0500

    fixing

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,13 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-
+console.log('process.env.DB_NAME', process.env.DB_NAME);
+console.log('process.env.DB_USER', process.env.DB_USER);
+console.log('process.env.DB_PASSWORD', process.env.DB_PASSWORD);
 let sequelize;
+
+const DB_NAME = 'blog_db';
+const DB_USER = 'root';
+const DB_PASSWORD = 'root';
 
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
