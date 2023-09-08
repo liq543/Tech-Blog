71f96c686abccd9b8bf11897b9c49a41033578d2
unknown <liq543@gmail.com>
2023-09-08 14:53:42 -0500

    controllers

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,123 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth')
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get("/post/:id", withAuth, async (req, res) => {
+
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+                {
+                    model: Comment,
+                    include: {
+                        model: User,
+                        attributes: ['username'],
+                    },
+                },
+            ],
+        });
+
+        const post = postData.get({ plain: true });
+
+        res.render('post', {
+            ...post,
+            logged_in: req.session.logged_in
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [
+                {
+                    model: Post,
+                    attributes: [
+                        'id',
+                        'text',
+                        'title',
+                        'date_created',
+                    ],
+                },
+            ],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('dashboard', {
+            ...user,
+            logged_in: true,
+        });
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+router.get('/create', async (req, res) => {
+    res.render('create', {
+        logged_in: true,
+    });
+});
+
+router.get('/update/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attribute: ['username'],
+                },
+            ],
+        });
+
+        const post = postData.get({ plain: true });
+
+        res.render('update', {
+            ...post,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get('/login', async (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('login');
+});
+
+module.exports = router;
+
