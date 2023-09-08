73067ddbacf21bd3eee1b8ff9006ed0f75ef22ae
unknown <liq543@gmail.com>
2023-09-08 14:54:12 -0500

    models

---
+++ models/index.js
@@ -0,0 +1,38 @@
+// Comment are related to Post
+// Post can have many Comment
+// Comment belongs to Post and User
+// Post belong to User
+// User can have many Post
+
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
